528a133baa5f985b641ed54f5b040545ba583542
Josep F. Hervás Fuertes <josep.hervas@gmail.com>
2023-11-21 23:02:35 +0100

    Refactored code

--- nsfw-scanner.py
+++ nsfw-scanner.py
@@ -6,17 +6,24 @@ import sys
 import os
 import shutil
 
-def parse_args() -> tuple[str, bool, Optional[str]]:
-    parser = argparse.ArgumentParser(description='Scans a file tree for NSFW files')
 
-    parser.add_argument('directory', type=str, help='Directory to start scanning from')
-    parser.add_argument('-r', '--recursive', action='store_true', help='Recursively scan subdirectories')
-    parser.add_argument('-t', '--target', type=str, default=None, help='Target directory where NSFW files will be moved')
+def parse_args() -> tuple[str, bool, Optional[str]]:
+    parser = argparse.ArgumentParser(
+        description='Scans a file tree for NSFW files')
+
+    parser.add_argument('directory', type=str,
+                        help='Directory to start scanning from')
+    parser.add_argument('-r', '--recursive', action='store_true',
+                        help='Recursively scan subdirectories')
+    parser.add_argument('-t', '--target', type=str, default=None,
+                        help='Target directory where NSFW files will be moved')
 
     args = parser.parse_args()
     return args.directory, args.recursive, args.target
 
 # Function to scan a directory for images
+
+
 def scan_directory(classifier: pipeline, directory: str, recursive: bool, target: Optional[str]) -> None:
     # Check if target directory exists
     if target:
@@ -48,34 +55,40 @@ def scan_directory(classifier: pipeline, directory: str, recursive: bool, target
 
             # Move image to target directory if NSFW
             if nsfw > 0.05 and nsfw < 0.15:
-                print(image_path + ' MAY have NSFW content with a mixed score of nsfw: ' + str(nsfw) + ' and normal: ' + str(normal) + '. A manual review is suggested.')
+                print(image_path + ' MAY have NSFW content with a mixed score of nsfw: ' +
+                      str(nsfw) + ' and normal: ' + str(normal) + '. A manual review is suggested.')
             elif nsfw >= 0.15 and nsfw < 0.5:
                 if target:
                     target_path = os.path.join(target, file)
                     # We move the file to the target path
                     os.rename(image_path, target_path)
 
-                    print (f"{image_path} very likely has NSFW content " + str(nsfw) + " vs " + str(normal) + ". Moved to " + target_path + ".")
+                    print(f"{image_path} very likely has NSFW content " + str(nsfw) +
+                          " vs " + str(normal) + ". Moved to " + target_path + ".")
                 else:
-                    print (f"{image_path} very likely has NSFW content " + str(nsfw) + " vs " + str(normal) + ".")
+                    print(f"{image_path} very likely has NSFW content " +
+                          str(nsfw) + " vs " + str(normal) + ".")
             elif nsfw >= 0.5:
                 if target:
                     target_path = os.path.join(target, file)
                     os.rename(image_path, target_path)
-                    print (f"{image_path} has NSFW content. Moving to {target_path}.")
+                    print(f"{image_path} has NSFW content. Moving to {
+                          target_path}.")
                 else:
-                    print (f"{image_path} has NSFW content.")
+                    print(f"{image_path} has NSFW content.")
             elif normal > 0.9:
                 print(image_path + ' is ok.')
             else:
-                print(image_path +" is ambiguous " + str(nsfw) + " vs " + str(normal) + ". A manual review is suggested.")
-        
+                print(image_path + " is ambiguous " + str(nsfw) +
+                      " vs " + str(normal) + ". A manual review is suggested.")
+
     # If recursive is set to True, scan subdirectories
     if recursive:
         for subdir in os.listdir(directory):
             subdir_path = os.path.join(directory, subdir)
             if os.path.isdir(subdir_path):
                 scan_directory(classifier, subdir_path, recursive, target)
+
 
 # Parse command line arguments
 directory, recursive, target = parse_args()
@@ -92,6 +105,7 @@ if target:
         exit(1)
 
 # Load the model
-classifier = pipeline('image-classification', model='Falconsai/nsfw_image_detection')
+classifier = pipeline('image-classification',
+                      model='Falconsai/nsfw_image_detection')
 
 scan_directory(classifier, directory, recursive, target)
