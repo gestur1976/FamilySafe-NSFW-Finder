10e04752b98a65d88478b7ad9ebdb1248943a08c
Josep F. Hervás Fuertes <josep.hervas@gmail.com>
2023-11-22 13:08:17 +0100

    Refactor the cose using proper naming conventions and adherence to PEP8 style guidelines.

--- README.md
+++ README.md
@@ -1,32 +1,29 @@
 # NSFW Image Scanner
 
 ## Description
-This script scans a specified directory for images and uses a classifier model to detect NSFW (Not Safe For Work) content. If NSFW content is detected, the script can optionally move these images to a target directory. It supports scanning directories recursively.
+This script scans a directory for images and uses a classifier model to detect NSFW (Not Safe For Work) content. If NSFW content is found, the script can optionally move these images to a specified directory. It supports recursive scanning of directories.
 
 ## Features
 - Scans specified directories for image files (JPG, JPEG, PNG, GIF).
-- Utilizes a machine learning model to classify images as NSFW or safe.
-- Moves detected NSFW images to a specified target directory.
-- Option to scan directories recursively.
+- Uses a machine learning model to classify images as NSFW or safe.
+- Can move detected NSFW images to a chosen directory.
+- Option for recursive directory scanning.
 
 ## Requirements
 - Python 3.x
-- PIL (Python Imaging Library)
-- Transformers Library
-- A pre-trained NSFW classification model (default: Falconsai/nsfw_image_detection)
+- Anaconda or Miniconda (for TensorFlow and Keras support)
 
 ## Installation
 
-
 1. Ensure Python 3.x is installed on your system.
-2. Clone this repository or download the script.
-3. Create a Conda environment and activate it:
+2. Clone the repository or download the script.
+3. Create a Conda environment:
 
 ```bash
 conda create -n nsfw_scanner python=3.9
 conda activate nsfw_scanner
 ```
-4. Set up conda environment:
+4. Set up the conda environment:
 
 ```bash
 conda install cuda-toolkit cudnn tensorflow -c "nvidia/label/cuda-11.8.0"
@@ -34,26 +31,28 @@ conda env config vars set LD_LIBRARY_PATH=$CONDA_PREFIX/lib
 conda deactivate
 conda activate nsfw_scanner
 ```
-4. Install the required Python packages:
+
+4. Install required Python packages:
 ```bash
 pip install -r requirements.txt
 ```
 
 ## Usage
-Before using the script activate the conda environment if it isn't activated.
 
-Run the script from the command line, specifying the source directory and optionally the destination folder to move the detected images:
+Activate the conda environment before using the script.
+
+Run the script via command line, specifying the source directory and, if desired, a destination folder for detected images:
 
 ```bash
 conda activate nsfw_scanner
-
 python nsfw-scanner.py [directory] [options]
-``````
+```
 
 ## Arguments
-- directory: The directory to start scanning from.
-- -r, --recursive: Recursively scan subdirectories.
-- [ -t, --target directory ]: Target directory where NSFW files will be moved (optional).
+
+- directory: The directory scan.
+- -r, --recursive: Scan subdirectories recursively.
+- [ -t, --target directory ]: Optional target directory for NSFW files.
 
 ## Example
 ```bash
@@ -62,12 +61,12 @@ python nsfw-scanner.py ~/Images --recursive --target ~/.nsfw_images
 
 ## Note
 
-- The accuracy of NSFW detection depends on the model used.
-- The script requires internet access to download the model upon first run.
+- NSFW detection accuracy depends on the model.
+- Internet access is required to download the model on first run.
 
 ## License
 
-This project is licensed under the MIT License - see the LICENSE file for details.
+Licensed under the MIT License - see the LICENSE file for details.
 
 ## Contribution
 
@@ -75,4 +74,4 @@ Contributions to the project are welcome. Please fork the repository and submit 
 
 ## Disclaimer
 
-This tool is for educational purposes only. The author is not responsible for misuse or for any damage that may occur.
+This tool is for educational use only. The author is not responsible for misuse or any resulting damage.
--- nsfw-scanner.py
+++ nsfw-scanner.py
@@ -1,111 +1,80 @@
 import argparse
-from typing import Optional
+import os
+import sys
+from typing import Optional, Tuple
 from PIL import Image
 from transformers import pipeline
-import sys
-import os
-import shutil
 
-
-def parse_args() -> tuple[str, bool, Optional[str]]:
-    parser = argparse.ArgumentParser(
-        description='Scans a file tree for NSFW files')
-
-    parser.add_argument('directory', type=str,
-                        help='Directory to start scanning from')
-    parser.add_argument('-r', '--recursive', action='store_true',
-                        help='Recursively scan subdirectories')
-    parser.add_argument('-t', '--target', type=str, default=None,
-                        help='Target directory where NSFW files will be moved')
+def parse_arguments() -> Tuple[str, bool, Optional[str]]:
+    parser = argparse.ArgumentParser(description='Scans a directory for NSFW files')
+    parser.add_argument('directory', type=str, help='Directory to start scanning from')
+    parser.add_argument('-r', '--recursive', action='store_true', help='Recursively scan subdirectories')
+    parser.add_argument('-t', '--target', type=str, default=None, help='Target directory for NSFW files')
 
     args = parser.parse_args()
     return args.directory, args.recursive, args.target
 
-# Function to scan a directory for images
+def scan_directory(classifier: pipeline, directory: str, recursive: bool, target_dir: Optional[str]) -> None:
+    if target_dir and not os.path.exists(target_dir):
+        return
 
-
-def scan_directory(classifier: pipeline, directory: str, recursive: bool, target: Optional[str]) -> None:
-    # Check if target directory exists
-    if target:
-        if not os.path.exists(target):
-            return
-    print("Scanning directori: " + directory)
-    # Get all files in directory
+    print(f"Scanning directory: {directory}")
     files = os.listdir(directory)
 
     for file in files:
-        # Check if file is an image
         if file.lower().endswith(('.jpg', '.jpeg', '.png', '.gif')):
-            # Get image path
             image_path = os.path.join(directory, file)
-            # Load image
             try:
                 image = Image.open(image_path)
             except OSError:
-                print(f'Error opening image: {image_path}', sys.stderr)
+                print(f'Error opening image: {image_path}', file=sys.stderr)
                 continue
-            # Check if image is NSFW
-            print("Checking " + image_path)
-            result = classifier(image)
-            for result_dict in result:
-                if result_dict['label'] == 'nsfw':
-                    nsfw = result_dict['score']
-                if result_dict['label'] == 'normal':
-                    normal = result_dict['score']
 
-            # Move image to target directory if NSFW
-            if nsfw > 0.05 and nsfw < 0.15:
-                print(image_path + ' MAY have NSFW content with a mixed score of nsfw: ' +
-                      str(nsfw) + ' and normal: ' + str(normal) + '. A manual review is suggested.')
-            elif nsfw >= 0.15 and nsfw < 0.5:
-                if target:
-                    target_path = os.path.join(target, file)
-                    # We move the file to the target path
-                    os.rename(image_path, target_path)
+            print(f"Checking {image_path}")
+            results = classifier(image)
+            nsfw_score, normal_score = 0, 0
 
-                    print(f"{image_path} very likely has NSFW content " + str(nsfw) +
-                          " vs " + str(normal) + ". Moved to " + target_path + ".")
-                else:
-                    print(f"{image_path} very likely has NSFW content " +
-                          str(nsfw) + " vs " + str(normal) + ".")
-            elif nsfw >= 0.5:
-                if target:
-                    target_path = os.path.join(target, file)
-                    os.rename(image_path, target_path)
-                    print(f"{image_path} has NSFW content. Moving to {
-                          target_path}.")
-                else:
-                    print(f"{image_path} has NSFW content.")
-            elif normal > 0.9:
-                print(image_path + ' is ok.')
-            else:
-                print(image_path + " is ambiguous " + str(nsfw) +
-                      " vs " + str(normal) + ". A manual review is suggested.")
+            for result in results:
+                if result['label'] == 'nsfw':
+                    nsfw_score = result['score']
+                if result['label'] == 'normal':
+                    normal_score = result['score']
 
-    # If recursive is set to True, scan subdirectories
+            target_path = os.path.join(target_dir, file) if target_dir else None
+            analyze_and_move(image_path, nsfw_score, normal_score, target_path)
+
     if recursive:
-        for subdir in os.listdir(directory):
-            subdir_path = os.path.join(directory, subdir)
-            if os.path.isdir(subdir_path):
-                scan_directory(classifier, subdir_path, recursive, target)
+        for subdir in [d for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))]:
+            scan_directory(classifier, os.path.join(directory, subdir), recursive, target_dir)
 
+def analyze_and_move(image_path: str, nsfw_score: float, normal_score: float, target_path: Optional[str]) -> None:
+    if nsfw_score > 0.5:
+        move_file(image_path, target_path, f"NSFW content detected. Moving to {target_path}.")
+    elif nsfw_score >= 0.15:
+        move_file(image_path, target_path, f"Likely NSFW content. Moving to {target_path}.")
+    elif nsfw_score > 0.05:
+        print(f"{image_path} may contain NSFW content. Manual review suggested.")
+    elif normal_score > 0.9:
+        print(f"{image_path} is classified as safe.")
+    else:
+        print(f"{image_path} is ambiguous. Manual review suggested.")
 
-# Parse command line arguments
-directory, recursive, target = parse_args()
+def move_file(source: str, destination: Optional[str], message: str) -> None:
+    if destination:
+        os.rename(source, destination)
+        print(message)
+    else:
+        print(f"{source} {message}")
 
-# Check if source directory exists
+directory, recursive, target = parse_arguments()
+
 if not os.path.exists(directory):
-    print(f'Source directory does not exist: {directory}', sys.stderr)
-    exit(1)
+    print(f'Source directory does not exist: {directory}', file=sys.stderr)
+    sys.exit(1)
 
-# If specified, check if target directory exists
-if target:
-    if not os.path.exists(target):
-        print(f'Target directory does not exist: {target}', sys.stderr)
-        exit(1)
+if target and not os.path.exists(target):
+    print(f'Target directory does not exist: {target}', file=sys.stderr)
+    sys.exit(1)
 
-# Load the model
-classifier = pipeline('image-classification',
-                      model='Falconsai/nsfw_image_detection')
-
+classifier = pipeline('image-classification', model='Falconsai/nsfw_image_detection')
 scan_directory(classifier, directory, recursive, target)
