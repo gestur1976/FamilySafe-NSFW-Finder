df51721ff50175f59298c13d7d727665cbd16e9d
Josep F. Hervás Fuertes <josep.hervas@gmail.com>
2024-08-02 16:55:57 +0200

    [WIP] Created the Classifier class to include the image processor model, device autodetection and image classification logic. Adding parameters to control the frequency of frames analyzed for video files.

--- nsfw-finder.py
+++ nsfw-finder.py
@@ -9,15 +9,43 @@ import tensorflow as tf
 import cv2
 
 
-def parse_arguments() -> Tuple[str, bool, Optional[str]]:
+def parse_arguments() -> Tuple[str, bool, Optional[str], Optional[float], Optional[int]]:
     parser = argparse.ArgumentParser(description='Scans a directory for NSFW files')
     parser.add_argument('directory', type=str, help='Directory to start scanning from')
     parser.add_argument('-r', '--recursive', action='store_true', help='Recursively scan subdirectories')
     parser.add_argument('-t', '--target', type=str, default=None, help='Target directory for NSFW files')
+    parser.add_argument('-d', '--delay', type=int, default=5, help='This option sets the time gap (in seconds) '
+                                                                   'between each frame analyzed from video files. The '
+                                                                   'default is 5.0 seconds, meaning only one out of '
+                                                                   'every five seconds will be processed. Set this to '
+                                                                   '0 if you want to analyze all frames.')
+    parser.add_argument('-f', '--frequency', type=int, help='This option controls how often frames are selected for '
+                                                            'analysis in a video file. A value of 1 means analyzing '
+                                                            'every single frame. If not specified, the default '
+                                                            'behavior is equivalent to using `-d 5`.')
 
     args = parser.parse_args()
-    return args.directory, args.recursive, args.target
+    return args.directory, args.recursive, args.target, args.delay, args.frequency
 
+class Classifier:
+    def __init__(self, model_name):
+        gpu_devices = tf.config.experimental.list_physical_devices("GPU")
+        for device in gpu_devices:
+            tf.config.experimental.set_memory_growth(device, True)
+        self.model = AutoModelForImageClassification.from_pretrained(model_name)
+        self.classifier = ViTImageProcessor.from_pretrained(model_name)
+        self.model.eval()
+        self.device = torch.device(
+            'cuda' if torch.cuda.is_available() else ('mps' if torch.backends.mps.is_available() else 'cpu'))
+        self.model.to(self.device)
+
+    def analyze_image(self, image):
+        inputs = self.classifier(images=image, return_tensors="pt").to(self.device)
+        with torch.no_grad():
+            results = self.model(**inputs)
+        log_items = results.logits
+        predicted_label = log_items.argmax(-1).item()
+        return self.model.config.id2label[predicted_label] == "nsfw"
 
 class NSFWFinder:
     def __init__(self):
@@ -28,7 +56,7 @@ class NSFWFinder:
         self.image_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.tif', '.webp')
         self.video_extensions = ('.mp4', '.avi', '.mov', '.mkv', '.wmv', '.flv', '.webm', '.mpeg', '.mpg', '.m4v')
 
-    def scan_directory(self, model, classifier, directory: str, recursive: bool, target_dir: Optional[str]) -> None:
+    def scan_directory(self, classifier, directory: str, recursive: bool, target_dir: Optional[str]) -> None:
         if target_dir and not os.path.exists(target_dir):
             return
 
@@ -39,13 +67,13 @@ class NSFWFinder:
             item_path = os.path.join(directory, item)
             if os.path.isfile(item_path):
                 if item.lower().endswith(self.image_extensions):
-                    self.process_image_file(model, classifier, item_path, target_dir)
+                    self.process_image_file(classifier, item_path, target_dir)
                 elif item.lower().endswith(self.video_extensions):
-                    self.process_video_file(model, classifier, item_path, target_dir)
+                    self.process_video_file(classifier, item_path, target_dir)
             elif recursive and os.path.isdir(item_path):
-                self.scan_directory(model, classifier, item_path, recursive, target_dir)
+                self.scan_directory(classifier, item_path, recursive, target_dir)
 
-    def process_image_file(self, model, classifier, image_path: str, target_dir: Optional[str]) -> None:
+    def process_image_file(self, classifier, image_path: str, target_dir: Optional[str]) -> None:
         try:
             image = Image.open(image_path)
         except (OSError, Exception) as e:
@@ -53,31 +81,24 @@ class NSFWFinder:
             return
 
         print(f"Checking {image_path}")
+        self.files_count += 1
+        if classifier.analyze_image(image_path):
+            target_path = os.path.join(target_dir, os.path.basename(image_path)) if target_dir else None
+            self.move_file(image_path, target_path, f"NSFW content detected. Moving to {target_path}.")
+            self.nsfw_count += 1
+        else:
+            print(f"{image_path} is classified as safe.")
+            self.normal_count += 1
 
-        inputs = classifier(images=image, return_tensors="pt").to(device)
-        with torch.no_grad():
-            results = model(**inputs)
-        log_items = results.logits
-        predicted_label = log_items.argmax(-1).item()
-        model.config.id2label[predicted_label]
-        attrs = vars(results)
-        print(predicted_label)
-        print(', '.join("%s: %s" % item for item in attrs.items()))
-
-        nsfw_score, normal_score = 0, 0
-        #for result in results:
-        #    if result['label'] == 'nsfw':
-        #        nsfw_score = result['score']
-        #    if result['label'] == 'normal':
-        #        normal_score = result['score']
-
-        #target_path = os.path.join(target_dir, os.path.basename(image_path)) if target_dir else None
-        #self.analyze_and_move(image_path, nsfw_score, normal_score, target_path)
-
-    def process_video_file(self, model, classifier, video_path: str, target_dir: Optional[str]) -> None:
+    def process_video_file(self, classifier, video_path: str, target_dir: Optional[str]) -> None:
         try:
             cap = cv2.VideoCapture(video_path)
+            frames_per_second = cap.get(cv2.CAP_PROP_FPS)   # Get fps of the video
             frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            if frames_per_second == 0:
+                print(f"Error: Unable to determine frames per second for {video_path}")
+                return
+
             frame_interval = max(1, frame_count // 10)  # Analyze 10 frames equally spaced
             frames_to_analyze = [i * frame_interval for i in range(10)]
 
@@ -91,15 +112,6 @@ class NSFWFinder:
                     continue
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 image = Image.fromarray(frame)
-                inputs = classifier(image=image, return_tensors="pt").to(device)
-                with torch.no_grad():
-                    results = model(**inputs)
-                results = classifier(image)
-                for result in results:
-                    if result['label'] == 'nsfw':
-                        nsfw_scores.append(result['score'])
-                    if result['label'] == 'normal':
-                        normal_scores.append(result['score'])
 
             cap.release()
 
@@ -112,26 +124,6 @@ class NSFWFinder:
             print(f'Error processing video: {video_path} - {e}', file=sys.stderr)
             return
 
-    def analyze_and_move(self, file_path: str, nsfw_score: float, normal_score: float,
-                         target_path: Optional[str]) -> None:
-        self.files_count += 1
-
-        if nsfw_score > 0.5:
-            self.nsfw_count += 1
-            self.move_file(file_path, target_path, f"NSFW content detected. Moving to {target_path}.")
-        elif nsfw_score >= 0.15:
-            self.nsfw_count += 1
-            self.move_file(file_path, target_path, f"Likely NSFW content. Moving to {target_path}.")
-        elif nsfw_score > 0.05:
-            self.suspect_count += 1
-            print(f"{file_path} may contain NSFW content. Manual review suggested.")
-        elif normal_score > 0.9:
-            self.normal_count += 1
-            print(f"{file_path} is classified as safe.")
-        else:
-            self.suspect_count += 1
-            print(f"{file_path} is ambiguous. Manual review suggested.")
-
     @staticmethod
     def move_file(source: str, destination: Optional[str], message: str) -> None:
         if destination:
@@ -142,8 +134,7 @@ class NSFWFinder:
 
 
 if __name__ == "__main__":
-    finder = NSFWFinder()
-    directory, recursive, target = parse_arguments()
+    directory, recursive, target, delay, frequency = parse_arguments()
 
     if not os.path.exists(directory):
         print(f'Source directory does not exist: {directory}', file=sys.stderr)
@@ -153,18 +144,11 @@ if __name__ == "__main__":
         print(f'Target directory does not exist: {target}', file=sys.stderr)
         sys.exit(1)
 
-    gpu_devices = tf.config.experimental.list_physical_devices("GPU")
-    for device in gpu_devices:
-        tf.config.experimental.set_memory_growth(device, True)
 
-    model = AutoModelForImageClassification.from_pretrained("Falconsai/nsfw_image_detection")
-    classifier = ViTImageProcessor.from_pretrained('Falconsai/nsfw_image_detection')
-    model.eval()
-    device = torch.device(
-        'cuda' if torch.cuda.is_available() else ('mps' if torch.backends.mps.is_available() else 'cpu'))
-    model.to(device)
+    classifier = Classifier("Falconsai/nsfw_image_detection")
+    finder = NSFWFinder()
 
-    finder.scan_directory(model, classifier, directory, recursive, target)
+    finder.scan_directory(classifier, directory, recursive, target)
 
     # Show statistics
     print("Scan results:")
